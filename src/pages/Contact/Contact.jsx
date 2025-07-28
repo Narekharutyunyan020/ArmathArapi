a47a77f1466768340064afc6103305e078afb2e8
Narekharutyunyan020/gyt-web <harutyunyannarek986@gmail.com>
2025-07-28 19:51:54 +0400

    contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
+import s from './Contact.module.scss';
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const [readMore, setReadMore] = useState(false);
 
-export default Contact
+  return (
+    <div className={s.wrapper}>
+      <button 
+        className={s.button} 
+        onClick={() => setReadMore(!readMore)}
+      >
+        {readMore ? 'Show Less' : 'Read More'}
+      </button>
+
+      {readMore && (
+        <p className={s.text}>
+          Այստեղ կարող լինել կոնտակտներ կամ եսիմ լիքը այլ բաներ կարելի թ անել
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Contact;
